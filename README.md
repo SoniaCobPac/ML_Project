7c3087418f48f358f1e63360658b20628eef6251
SoniaCobPac <soniacobo7@hotmail.com>
2021-07-06 21:22:59 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ML_Project
+### **Generating Music with Machine Learning**
+
+---------
+
+**Author:**
+Sonia Cobo Pacios
+07/2021
+The Bridge - The Bridge Digital Talent
+
+---------
+
+
+
+---------
+
+**Project description:**
+
+---------
+
+
+---------
+
+**Project structure:**
+
+---------
+
+
+
+
+---------
+
+**Project conclusion:**
+
+---------
+
---
+++ src/utils/folders_tb.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+def read_json(path, filename):
+    """
+    Read a json and return a object created from it.
+    Args:
+        file path and file name
+    
+    Returns: json object.
+    """
+    try:
+        with open(path + os.sep + filename +".json", "r+", encoding="latin-1") as outfile:
+            json_readed = json.load(outfile)
+        return json_readed
+    except Exception as error:
+        raise ValueError(error)
